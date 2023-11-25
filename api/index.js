512d7754975343497373eb4b33e8fbc94a6097c5
Athul Vijayan <web.wizard.athul@gmail.com>
2023-11-26 02:01:00 +0530

    connected to DB

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import { log } from 'console';
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{console.log('Connected to DB');})
+.catch((error)=>{console.log(error);})
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-
-import './App.css'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 function App() {
 
   return (
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/about' element={<About/>}/>
+          </Routes>
+        </BrowserRouter>
   )
 }
 
