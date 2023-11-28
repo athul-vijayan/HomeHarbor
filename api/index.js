272204b26f13fe3e03e4397eec8c4b094263683e
Athul Vijayan <web.wizard.athul@gmail.com>
2023-11-29 01:46:55 +0530

    authentication ready

--- api/index.js
+++ api/index.js
@@ -1,14 +1,33 @@
-import { log } from 'console';
+
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{console.log('Connected to DB');})
 .catch((error)=>{console.log(error);})
 
+
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
+})
+
+app.use(express.json())
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode||500
+    const message=err.message||'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+        message1:'from index.js err handler'
+    })
 })
