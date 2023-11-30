c0e5d9a365322c4a674fc968f1c95328e1001b0a
Athul Vijayan <web.wizard.athul@gmail.com>
2023-11-30 16:59:16 +0530

    create listing page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/createListing'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
             <Route path='/sign-up' element={<SignUp/>}/>
             <Route element={<PrivateRoute/>}>
               <Route path='/profile' element={<Profile/>}/>
+              <Route path='/create-listing' element={<CreateListing/>}/>
             </Route>
             <Route path='/about' element={<About/>}/>
           </Routes>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   updateUserStart,
   updateUserSuccess,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -190,6 +191,7 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link to={"/create-listing"} className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
